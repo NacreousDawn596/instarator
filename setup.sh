946c96390aab93e6ef70a3fbe54b25983808b444
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2021-12-08 22:43:59 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+echo "downloading dependencies..."
+sleep 1
+clear
+sudo apt-get install python3 python3-pip
+clear
+sudo pacman -S python3 python3-pip
+clear
+sudo apt-get install python3 python3-pip
+clear
+pip install -r requirements.txt
+clear 
+echo 'done!'
