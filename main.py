e595a170a66d3eaca31978ec0fd82bba1a586922
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2021-12-08 23:03:15 +0100

    a little upgrade

--- main.py
+++ main.py
@@ -4,15 +4,24 @@ import numpy
 usernames = input('tell me the usernames ;)\n->').split(' ')
 names = []
 followers = []
+fig, ax = plt.subplots()
 for username in usernames:
 	print(username, end=': ')
 	count = requests.get(f'https://www.instagram.com/{username}/?__a=1').json()["graphql"]["user"]['edge_followed_by']["count"]
 	print(count)
 	names.append(username)
 	followers.append(count)
-
 names = numpy.array(names)
 followers = numpy.array(followers)
-
-plt.bar(names, followers)
+labels = followers
+data = plt.bar(names, followers)
+def autolabel(rects):
+    for idx,rect in enumerate(data):
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
+                labels[idx],
+                ha='center', va='bottom', rotation=0)
+autolabel(data)
+plt.ylim(0,max(followers)*1.05)
+plt.title('Instagram Followers Comparator.')
 plt.show()
