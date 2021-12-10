9a96016682138a046eac0bac00ef7a450e39bd58
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2021-12-10 15:06:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,9 @@ for username in usernames:
 	print(f"{username} ", requests.get(f'https://www.instagram.com/{username}/?__a=1').json()["graphql"]["user"]['edge_followed_by']["count"])
 	names.append(username)
 	followers.append(requests.get(f'https://www.instagram.com/{username}/?__a=1').json()["graphql"]["user"]['edge_followed_by']["count"])
-names, followers, labels= numpy.array(names), numpy.array(followers), followers
+names, followers, labels = numpy.array(names), numpy.array(followers), followers
 def autolabel(rects):
-    for idx,rect in enumerate(plt.bar(names, followers)):
-        ax.text(rect.get_x() + rect.get_width()/2., 1.05*rect.get_height(),
-                labels[idx],
-                ha='center', va='bottom', rotation=0)
+    [ax.text(rect.get_x() + rect.get_width()/2., 1.05*rect.get_height(), labels[idx], ha='center', va='bottom', rotation=0) for idx,rect in enumerate(plt.bar(names, followers))] 
 autolabel(plt.bar(names, followers))
 plt.ylim(0,max(followers)*1.05)
 plt.title('Instagram Followers Comparator.')
