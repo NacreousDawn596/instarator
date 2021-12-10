d65175e72812a64936ab69b10fc895677b8dacaa
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2021-12-10 15:03:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,13 @@ import requests
 import matplotlib.pyplot as plt 
 import numpy
 usernames = input('tell me the usernames ;)\n->').split(' ')
-names = []
-followers = []
+names = followers = []
 fig, ax = plt.subplots()
 for username in usernames:
 	print(f"{username} ", requests.get(f'https://www.instagram.com/{username}/?__a=1').json()["graphql"]["user"]['edge_followed_by']["count"])
 	names.append(username)
 	followers.append(requests.get(f'https://www.instagram.com/{username}/?__a=1').json()["graphql"]["user"]['edge_followed_by']["count"])
-names = numpy.array(names)
-followers = numpy.array(followers)
-labels = followers
+names, followers, labels= numpy.array(names), numpy.array(followers), followers
 def autolabel(rects):
     for idx,rect in enumerate(plt.bar(names, followers)):
         ax.text(rect.get_x() + rect.get_width()/2., 1.05*rect.get_height(),
